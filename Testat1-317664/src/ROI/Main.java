76b8fae32bc1521e2335f490ae7f2dbfa61631f1
Cenedikt <44801967+Cenedikt@users.noreply.github.com>
2019-03-26 17:16:29 +0100

    Add files via upload

---
+++ Testat1-317664/src/ROI/Main.java
@@ -0,0 +1,26 @@
+package ROI;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author BenedictGoldhorn 317664
+ */
+public class Main 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        //Variables   
+    }
+    
+}
